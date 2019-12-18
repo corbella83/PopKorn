04fc268bb8237cc2a56eacbe5b3a153a921adae2
corbella83 <corbella83@gmail.com>
2019-12-18 08:30:03 +0100

    add missing documentation

--- popkorn/src/main/kotlin/cc/popkorn/core/model/Empty.kt
+++ popkorn/src/main/kotlin/cc/popkorn/core/model/Empty.kt
@@ -1,3 +1,10 @@
 package cc.popkorn.core.model
 
+/**
+ * Model class to be used in any constructor/method for Injectable objects
+ * so you can define multiple constructors for different environments with the same parameters
+ *
+ * @author Pau Corbella
+ * @since 1.3.0
+ */
 class Empty
--- popkorn/src/main/kotlin/cc/popkorn/core/model/Environment.kt
+++ popkorn/src/main/kotlin/cc/popkorn/core/model/Environment.kt
@@ -1,3 +1,10 @@
 package cc.popkorn.core.model
 
+/**
+ * Model class to be used in any constructor/method for Injectable objects
+ * so you can get the environment that is using at the moment
+ *
+ * @author Pau Corbella
+ * @since 1.3.0
+ */
 class Environment(val value: String?)
