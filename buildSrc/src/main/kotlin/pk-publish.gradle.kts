7c9d5671a39854c1730d80ab7721486f46fb8980
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2021-01-06 14:03:17 +0100

    Publish setup in `local.properties` is not required to build the project

--- buildSrc/src/main/kotlin/pk-publish.gradle.kts
+++ buildSrc/src/main/kotlin/pk-publish.gradle.kts
@@ -1,69 +1,96 @@
-val properties = java.util.Properties()
-properties.load(rootProject.file("local.properties").inputStream())
-
-val isReleaseVersion = !version.toString().endsWith("SNAPSHOT")
-
 plugins {
     `maven-publish`
     signing
 }
 
+val localPropertiesFile = rootProject.file("local.properties")
 
-project.extra.set("signing.keyId", properties.getProperty("signing.keyId"))
-project.extra.set("signing.secretKeyRingFile", properties.getProperty("signing.secretKeyRingFile"))
-project.extra.set("signing.password", properties.getProperty("signing.password"))
+if (localPropertiesFile.exists()) {
+    val properties = java.util.Properties()
+    properties.load(localPropertiesFile.inputStream())
 
+    val signingKeyId = properties.getProperty("signing.keyId")
+    val signingSecretKeyRingFile = properties.getProperty("signing.secretKeyRingFile")
+    val signingPassword = properties.getProperty("signing.password")
 
-publishing {
+    val nexusPro = properties.getProperty("nexus.pro_url")
+    val nexusSnapshot = properties.getProperty("nexus.snap_url")
 
-    publications.configureEach {
-        if (this is MavenPublication) {
-            val publicName = "${rootProject.name} ${name.capitalize()}"
-            pom {
-                name.set(publicName)
-                description.set("Powerful Dependency Injector for Kotlin")
-                url.set("https://github.com/corbella83/PopKorn")
-                licenses {
-                    license {
-                        name.set("The Apache Software License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                        distribution.set("repo")
+    val nexusUsername = properties.getProperty("nexus.username")
+    val nexusPassword = properties.getProperty("nexus.password")
+
+    val publishingProperties = listOf(
+        signingKeyId,
+        signingSecretKeyRingFile,
+        signingPassword,
+        nexusPro,
+        nexusSnapshot,
+        nexusUsername,
+        nexusPassword
+    )
+
+    if (publishingProperties.all { property -> property != null }) {
+        val isReleaseVersion = !version.toString().endsWith("SNAPSHOT")
+
+        project.extra.set("signing.keyId", signingKeyId)
+        project.extra.set("signing.secretKeyRingFile", signingSecretKeyRingFile)
+        project.extra.set("signing.password", signingPassword)
+
+        publishing {
+
+            publications.configureEach {
+                if (this is MavenPublication) {
+                    val publicName = "${rootProject.name} ${name.capitalize()}"
+                    pom {
+                        name.set(publicName)
+                        description.set("Powerful Dependency Injector for Kotlin")
+                        url.set("https://github.com/corbella83/PopKorn")
+                        licenses {
+                            license {
+                                name.set("The Apache Software License, Version 2.0")
+                                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                                distribution.set("repo")
+                            }
+                        }
+                        developers {
+                            developer {
+                                id.set("corbella83")
+                                name.set("Pau Corbella")
+                            }
+                        }
+                        scm {
+                            connection.set("scm:git:git://github.com/corbella83/PopKorn.git")
+                            developerConnection.set("scm:git:ssh://git@github.com/corbella83/PopKorn.git")
+                            url.set("https://github.com/corbella83/PopKorn")
+                        }
                     }
                 }
-                developers {
-                    developer {
-                        id.set("corbella83")
-                        name.set("Pau Corbella")
+            }
+
+            repositories {
+                maven {
+                    val releasesRepoUrl = uri(nexusPro)
+                    val snapshotsRepoUrl = uri(nexusSnapshot)
+                    url = if (isReleaseVersion) releasesRepoUrl else snapshotsRepoUrl
+                    credentials {
+                        username = nexusUsername
+                        password = nexusPassword
                     }
-                }
-                scm {
-                    connection.set("scm:git:git://github.com/corbella83/PopKorn.git")
-                    developerConnection.set("scm:git:ssh://git@github.com/corbella83/PopKorn.git")
-                    url.set("https://github.com/corbella83/PopKorn")
                 }
             }
         }
-    }
 
-    repositories {
-        maven {
-            val releasesRepoUrl = uri(properties.getProperty("nexus.pro_url"))
-            val snapshotsRepoUrl = uri(properties.getProperty("nexus.snap_url"))
-            url = if (isReleaseVersion) releasesRepoUrl else snapshotsRepoUrl
-            credentials {
-                username = properties.getProperty("nexus.username")
-                password = properties.getProperty("nexus.password")
-            }
+        tasks.withType<Sign>().configureEach {
+            onlyIf { isReleaseVersion }
         }
+
+        signing {
+            sign(publishing.publications)
+        }
+    } else {
+        println(
+            "Warning: One or more properties required to publish `${project.name}` to Maven Central has not been " +
+                    "added to local.properties file"
+        )
     }
-
 }
-
-tasks.withType<Sign>().configureEach {
-    onlyIf { isReleaseVersion }
-}
-
-signing {
-    sign(publishing.publications)
-}
-
