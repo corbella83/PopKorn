d74bfcd2fb7d5a2335c8f92da28e72c70ef82ac5
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2021-01-13 14:03:05 +0100

    ViewModel support until API 14

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -5,7 +5,7 @@ fun BaseExtension.androidConfig() {
     compileSdkVersion(30)
 
     defaultConfig {
-        minSdkVersion(15)
+        minSdkVersion(14)
         targetSdkVersion(30)
         versionCode = 1
         versionName = "1.0"
